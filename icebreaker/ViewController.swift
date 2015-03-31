e18e83bb998b2c6d7cb4a0488f64fb1533efa144
KoJoVe <JoaoVicente@MacBook-Pro-de-Joao-2.local>
2015-03-31 10:05:24 -0300

    Initial Commit

---
+++ icebreaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  icebreaker
+//
+//  Created by Joao Nassar Galante Guedes on 31/03/15.
+//  Copyright (c) 2015 Joao Nassar Galante Guedes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("Teste")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
